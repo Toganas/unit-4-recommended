08e8e6fd470dc88dab167afb49105a9c53c8c73e
Teddy Brewer <tebrewer@jbi-ltd.com>
2019-06-12 14:38:40 -0500

    random numbers for gems and target

---
+++ assets/javascript/game.js
@@ -0,0 +1,9 @@
+var gem1 = Math.floor(Math.random() * 12 + 1);
+var gem2 = Math.floor(Math.random() * 12 + 1); 
+var gem3 = Math.floor(Math.random() * 12 + 1); 
+var gem4 = Math.floor(Math.random() * 12 + 1);
+var target = Math.floor(Math.random()*102+19);
+
+
+
+console.log (target);
